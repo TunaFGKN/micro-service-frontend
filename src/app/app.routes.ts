fa5b99ce89a239175f13db7cafeb0b76e43ea471
Tuna F. <swaxztn@gmail.com>
2025-07-03 16:04:16 +0300

    First Configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { Layout } from './components/layout/layout';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: Login
+    },
+    {
+        path: "",
+        component: Layout,
+        children: [
+            {
+                path: "",
+                component: Home
+            },
+            {
+                path: "home",
+                component: Home
+            },
+            {
+                path: "about",
+                component: About
+            },
+            {
+                path: "contact",
+                component: Contact
+            },
+            {
+                path: "contact/:params",
+                component: Contact
+            }
+        ]
+    } 
+];
